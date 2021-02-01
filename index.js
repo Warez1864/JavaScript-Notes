fde7f4ee17c02ec326af70407eea8cc26b0a7e17
Warez1864 <warez1864@gmail.com>
2021-02-01 22:17:11 +0100

    Extra Notes

--- index.js
+++ index.js
@@ -2,25 +2,27 @@
 // Set the variable var1 to a string type
 let var1 = ""
 
-// Set the variable var2 to an type
+// Change the type of the variable var1 from a string to an integer
+var1 = 25;
+
+// Set the variable var2 to an integer type
 let var2 = 1
 
-// Set the variable var3 to an s
+// Set the variable var3 to a float type
 let var3 = 2.5
 
+// Make an empty object called person
 let person = {
 
 }
 
+// Make an array with some numbers
 let var4 = [
     1, 2, 3, 4
 ]
 
-const con1
-
-var1 = 25;
-
-
-
+// Add the number 5 to the array
 var4.push(5);
 
+// Make a constant called con1
+const con1
